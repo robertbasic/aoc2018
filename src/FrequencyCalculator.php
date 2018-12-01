3c1b7e564860bdcdaf645de340a6905ed4d86c3c
Robert Basic <robertbasic.com@gmail.com>
2018-12-01 08:37:38 +0100

    Day 1, puzzle 2

--- src/FrequencyCalculator.php
+++ src/FrequencyCalculator.php
@@ -10,8 +10,32 @@ class FrequencyCalculator
         return array_sum($frequencies);
     }
 
+    public function calculateLoopedFrequency(string $frequencies): int
+    {
+        $frequencies = $this->castFrequencies($frequencies);
+
+        $currentFrequency = 0;
+        $seenFrequencies = [];
+
+        while (true) {
+            foreach ($frequencies as $frequency) {
+                if (!isset($seenFrequencies[$currentFrequency])) {
+                    $seenFrequencies[$currentFrequency] = 1;
+                } else {
+                    $seenFrequencies[$currentFrequency] += 1;
+                }
+
+                if ($seenFrequencies[$currentFrequency] === 2) {
+                    return $currentFrequency;
+                }
+
+                $currentFrequency += $frequency;
+            }
+        }
+    }
+
     private function castFrequencies(string $frequencies): array
     {
-        return explode(',', $frequencies);
+        return array_map('trim', explode(',', $frequencies));
     }
 }
--- tests/Day1Test.php
+++ tests/Day1Test.php
@@ -6,6 +6,9 @@ use PHPUnit\Framework\TestCase;
 
 class Day1Test extends TestCase
 {
+    /**
+     * @var FrequencyCalculator
+     */
     private $frequencyCalculator;
 
     public function setup()
@@ -29,13 +32,38 @@ class Day1Test extends TestCase
      */
     public function resulting_frequency_is_calculated_with_actual_input()
     {
-        $input = file_get_contents('./tests/inputs/day1_1.txt');
+        $input = file_get_contents(__DIR__ . '/inputs/day1_1.txt');
 
         $frequencies = str_replace("\n", ",", $input);
 
         $resultingFrequency = $this->frequencyCalculator->calculate($frequencies);
 
         $this->assertSame(411, $resultingFrequency);
+    }
+
+    /**
+     * @test
+     * @dataProvider loopingFrequencies
+     */
+    public function looping_frequency_is_calculated($frequencies, $expectedLoopedFrequency)
+    {
+        $loopedFrequency = $this->frequencyCalculator->calculateLoopedFrequency($frequencies);
+
+        $this->assertSame($expectedLoopedFrequency, $loopedFrequency);
+    }
+
+    /**
+     * @test
+     */
+    public function looping_frequency_is_calculated_with_actual_input()
+    {
+        $input = file_get_contents(__DIR__ . '/inputs/day1_1.txt');
+
+        $frequencies = str_replace("\n", ",", rtrim($input, "\n"));
+
+        $loopedFrequency = $this->frequencyCalculator->calculateLoopedFrequency($frequencies);
+
+        $this->assertSame(56360, $loopedFrequency);
     }
 
     public function frequencies(): array
@@ -57,6 +85,32 @@ class Day1Test extends TestCase
                 '-1, -2, -3',
                 -6
             ],
+        ];
+    }
+
+    public function loopingFrequencies(): array
+    {
+        return [
+            [
+                '+1, -2, +3, +1',
+                2
+            ],
+            [
+                '+1, -1',
+                0
+            ],
+            [
+                '+3, +3, +4, -2, -4',
+                10
+            ],
+            [
+                '-6, +3, +8, +5, -6',
+                5
+            ],
+            [
+                '+7, +7, -2, -7, -4',
+                14
+            ]
         ];
     }
 }
